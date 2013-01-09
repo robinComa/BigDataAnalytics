5717be02e787e116f2d7598c83d7991ded52e851
robinComa <robin.coma.public@gmail.com>
2013-01-09 18:53:06 +0100

    Angularjs replace Backbone

--- WebContent/src/views.js
+++
@@ -1,7 +0,0 @@
-var DataView = Backbone.View.extend({
-	tagName : "div",
-	className : "data-row alert alert-success",
-	render : function() {
-		return $(this.el).html(_.template($('#data-template').text())(this.model.toJSON()));
-	}
-});
